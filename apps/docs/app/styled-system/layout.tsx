25da8e1a1235eb6214e716e162ee46dd4fdb1592
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-03-13 16:57:13 -0400

    update the documentation

--- apps/docs/app/styled-system/layout.tsx
+++ apps/docs/app/styled-system/layout.tsx
@@ -1,10 +1,16 @@
+import { allStyledSystems } from "@/.contentlayer/generated";
 import type { ReactNode } from "react";
-import Wrapper from "../ui/layout/wrapper/Wrapper";
+import getPageLinks from "../lib/getPageLinks";
+import { SidebarLayout } from "../ui/layout/sidebarLayout";
 
 export default function StyledSystemLayout({ children }: { children: ReactNode }) {
+    const allLinks = getPageLinks(allStyledSystems, {
+        order: ["overview"]
+    });
+
     return (
-        <Wrapper>
+        <SidebarLayout links={allLinks}>
             {children}
-        </Wrapper>
+        </SidebarLayout>
     );
 }
