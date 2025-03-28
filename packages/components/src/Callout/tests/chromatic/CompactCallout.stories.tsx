761618a369112f5943cf78df5cbe66d1f52e9e89
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-03-28 16:01:35 -0400

    fix compact callout

--- packages/components/src/Callout/tests/chromatic/CompactCallout.stories.tsx
+++ packages/components/src/Callout/tests/chromatic/CompactCallout.stories.tsx
@@ -7,7 +7,10 @@ import { CompactCallout } from "../../src/index.ts";
 
 const meta = {
     title: "Components/Callout/Compact",
-    component: CompactCallout
+    component: CompactCallout,
+    args: {
+        children: <Content>Callout content</Content>
+    }
 } satisfies Meta<typeof CompactCallout>;
 
 export default meta;
@@ -15,93 +18,86 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default = {
-    render: props => (
-        <CompactCallout {...props}>
-            <Content>Callout content</Content>
-        </CompactCallout>
-    )
 } satisfies Story;
 
 export const Inline = {
-    ...Default,
     args: {
         fillStyle: "subtleFill"
     }
 } satisfies Story;
 
 export const Information = {
-    ...Default,
     args: {
         variant: "information"
     }
 } satisfies Story;
 
 export const Warning = {
-    ...Default,
     args: {
         variant: "warning"
     }
 } satisfies Story;
 
 export const Success = {
-    ...Default,
     args: {
         variant: "success"
     }
 } satisfies Story;
 
 export const Upsell = {
-    ...Default,
     args: {
         variant: "upsell"
     }
 } satisfies Story;
 
 export const WithClose = {
-    ...Default,
     args: {
         onClose: () => alert("Closed")
     }
 } satisfies Story;
 
 export const WithCta = {
-    render: props => (
-        <CompactCallout {...props}>
-            <Content>Callout content</Content>
-            <Button variant="secondary" size="sm">Label</Button>
-        </CompactCallout>
-    )
+    args: {
+        children: (
+            <>
+                <Content>Callout content</Content>
+                <Button>Label</Button>
+            </>
+        )
+    }
 } satisfies Story;
 
 export const WithButtonAndCta = {
-    render: props => (
-        <CompactCallout {...props}>
-            <Content>Callout content</Content>
-            <Button variant="secondary" size="sm">Label</Button>
-        </CompactCallout>
-    ),
     args: {
-        onClose: () => alert("Closed")
+        onClose: () => alert("Closed"),
+        children: (
+            <>
+                <Content>Callout content</Content>
+                <Button>Label</Button>
+            </>
+        )
     }
 } satisfies Story;
 
 export const WithLink = {
-    render: props => (
-        <CompactCallout {...props}>
-            <Content>Callout content</Content>
-            <Link variant="secondary" size="sm">Label</Link>
-        </CompactCallout>
-    )
+    args: {
+        children: (
+            <>
+                <Content>Callout content</Content>
+                <Link>Label</Link>
+            </>
+        )
+    }
 } satisfies Story;
 
 export const WithButtonAndLink = {
-    render: props => (
-        <CompactCallout {...props}>
-            <Content>Callout content</Content>
-            <Link variant="secondary" size="sm">Label</Link>
-        </CompactCallout>
-    ),
     args: {
-        onClose: () => alert("Closed")
+        onClose: () => alert("Closed"),
+        children: (
+            <>
+                <Content>Callout content</Content>
+                <Link>Label</Link>
+            </>
+        )
     }
 } satisfies Story;
