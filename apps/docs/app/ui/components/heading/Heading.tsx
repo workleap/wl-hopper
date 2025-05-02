e08f1bfec02bf580bb9dbf5732abf3df48afa89f
Victor Trinh <victor.trinh@gsoft.com>
2025-05-02 11:01:09 -0400

    feat(doc): Remove preview only banner from Hopper

--- apps/docs/app/ui/components/heading/Heading.tsx
+++ apps/docs/app/ui/components/heading/Heading.tsx
@@ -6,7 +6,6 @@ import Title from "@/components/title/Title.tsx";
 import { ThemeContext } from "@/context/theme/ThemeProvider";
 import { Callout, type ColorScheme, Content, Heading as HopperHeading, HopperProvider } from "@hopper-ui/components";
 import clsx from "clsx";
-import Link from "next/link";
 import { useContext } from "react";
 import "./heading.css";
 
@@ -24,9 +23,6 @@ const Heading = ({ title, tag, alpha, className, description, links }: HeadingPr
 
     return (
         <div className={clsx("hd-heading", className)}>
-            <div className="hd-heading__banner">
-                <p><span className="hd-heading__banner-title">Preview Only: </span>These components are for reference only. Continue using <Link className="hd-heading__banner-link" href="https://wl-orbiter-website.netlify.app/">Orbiter</Link> in production until further notice.</p>
-            </div>
             <div className="hd-heading__title">
                 <Title level={1}>{title}</Title>
                 {tag && <Tag>{tag}</Tag>}
