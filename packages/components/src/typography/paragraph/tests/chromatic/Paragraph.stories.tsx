86307098190ee00a5c70f4a1481b27bd7afc9485
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-05-27 10:44:47 -0400

    fix tests

--- packages/components/src/typography/paragraph/tests/chromatic/Paragraph.stories.tsx
+++ packages/components/src/typography/paragraph/tests/chromatic/Paragraph.stories.tsx
@@ -19,12 +19,12 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
     render: () => (
         <>
-            <Paragraph size="2xl">If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
-            <Paragraph size="xl">If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
-            <Paragraph size="lg">If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
-            <Paragraph>If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
-            <Paragraph size="sm">If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
-            <Paragraph size="xs">If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
+            <Paragraph size="2xl">One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
+            <Paragraph size="xl">One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
+            <Paragraph size="lg">One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
+            <Paragraph>One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
+            <Paragraph size="sm">One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
+            <Paragraph size="xs">One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
         </>
     )
 };
@@ -32,12 +32,12 @@ export const Default: Story = {
 export const WithLink: Story = {
     render: () => (
         <>
-            <Paragraph size="2xl">If two pieces of the same type of <Link href="#">metal touch</Link> in space they <Link href="https://www.workleap.com" isExternal>will permanently</Link> bond.</Paragraph>
-            <Paragraph size="xl">If two pieces of the same type of <Link href="#">metal touch</Link> in space they <Link href="https://www.workleap.com" isExternal>will permanently</Link> bond.</Paragraph>
-            <Paragraph size="lg">If two pieces of the same type of <Link href="#">metal touch</Link> in space they <Link href="https://www.workleap.com" isExternal>will permanently</Link> bond.</Paragraph>
-            <Paragraph>If two pieces of the same type of <Link href="#">metal touch</Link> in space they <Link href="https://www.workleap.com" isExternal>will permanently</Link> bond.</Paragraph>
-            <Paragraph size="sm">If two pieces of the same type of <Link href="#">metal touch</Link> in space they <Link href="https://www.workleap.com" isExternal>will permanently</Link> bond.</Paragraph>
-            <Paragraph size="xs">If two pieces of the same type of <Link href="#">metal touch</Link> in space they <Link href="https://www.workleap.com" isExternal>will permanently</Link> bond.</Paragraph>
+            <Paragraph size="2xl">One simple employee experience platform to boost <Link href="#">engagement</Link>, drive performance, and <Link href="https://www.workleap.com" isExternal>develop</Link> your people bond.</Paragraph>
+            <Paragraph size="xl">One simple employee experience platform to boost <Link href="#">engagement</Link>, drive performance, and <Link href="https://www.workleap.com" isExternal>develop</Link> your people bond.</Paragraph>
+            <Paragraph size="lg">One simple employee experience platform to boost <Link href="#">engagement</Link>, drive performance, and <Link href="https://www.workleap.com" isExternal>develop</Link> your people bond.</Paragraph>
+            <Paragraph>One simple employee experience platform to boost <Link href="#">engagement</Link>, drive performance, and <Link href="https://www.workleap.com" isExternal>develop</Link> your people bond.</Paragraph>
+            <Paragraph size="sm">One simple employee experience platform to boost <Link href="#">engagement</Link>, drive performance, and <Link href="https://www.workleap.com" isExternal>develop</Link> your people bond.</Paragraph>
+            <Paragraph size="xs">One simple employee experience platform to boost <Link href="#">engagement</Link>, drive performance, and <Link href="https://www.workleap.com" isExternal>develop</Link> your people bond.</Paragraph>
         </>
     )
 };
@@ -48,9 +48,9 @@ export const Styling: Story = {
     },
     render: () => (
         <>
-            <Paragraph border="warning">If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
-            <Paragraph className="bg-red">If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
-            <Paragraph style={{ backgroundColor: "red" }}>If two pieces of the same type of metal touch<br />in space they will permanently bond.</Paragraph>
+            <Paragraph border="warning">One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
+            <Paragraph className="bg-red">One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
+            <Paragraph style={{ backgroundColor: "red" }}>One simple employee experience platform to boost engagement,<br /> drive performance, and develop your people.</Paragraph>
         </>
     )
 };
