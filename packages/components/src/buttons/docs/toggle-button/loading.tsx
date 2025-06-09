7d62e98dae827fa89c4fecf52a1b91d2d0732955
Victor Trinh <victor.trinh@gsoft.com>
2025-06-09 16:41:08 -0400

    PR comments and fix build

--- packages/components/src/buttons/docs/toggle-button/loading.tsx
+++ packages/components/src/buttons/docs/toggle-button/loading.tsx
@@ -5,8 +5,11 @@ export default function Example() {
         <Inline>
             <ToggleButton isLoading variant="primary">Save</ToggleButton>
             <ToggleButton isLoading variant="secondary">Save</ToggleButton>
-            <ToggleButton isLoading variant="tertiary">Save</ToggleButton>
+            <ToggleButton isLoading variant="danger">Save</ToggleButton>
             <ToggleButton isLoading variant="upsell">Save</ToggleButton>
+            <ToggleButton isLoading variant="ghost-primary">Save</ToggleButton>
+            <ToggleButton isLoading variant="ghost-secondary">Save</ToggleButton>
+            <ToggleButton isLoading variant="ghost-danger">Save</ToggleButton>
         </Inline>
     );
 }
