4916ac8755f65691674bb6156903c9d936d83c7c
Mahmoud Moravej <mahmoud.moravej@workleap.com>
2025-07-28 16:33:36 -0400

    updates

--- apps/mcp-server/src/prompts.ts
+++ apps/mcp-server/src/prompts.ts
@@ -15,13 +15,14 @@ export function prompts(server: McpServer) {
                 text:
                 `
 Your role:
-Assume you are an expert UI/UX designer and frontend developer. Follow modern industry standards for spacing, layout, and visual hierarchy.
+Assume you are an expert UI/UX designer and an experienced frontend developer who is picky on design details. Follow modern industry standards for spacing, layout, and visual hierarchy.
 
 Rules to follow:
-- **Important** Follow provided guides and best practices from Hopper for styling, tokens, icons, and components. Use the provided tools to get the guides.
+- **Important** Follow styling, tokens, icons, and components.
+- NEVER EVER use emojis or special characters as icons. USE PROVIDED ICONS.
+- NEVER EVER use raw HTML(e.g. div, span, table, etx.) or Style(e.g. style={margin: 13px}). Use Hopper components and styles.
+- Button with icons should ALWAYS use Text component to wrap text.
 - Always consider responsiveness and accessibility.
-- Use Typescript and tsx format.
-- Use pnpm all the times.
 
 Task:
 ${query}
