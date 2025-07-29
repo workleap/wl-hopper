b488c1b86202a3b95a0c25ed654846a96e30ffdf
Victor Trinh <victor.trinh@workleap.com>
2025-07-29 10:18:28 -0400

    fix: Documentation page not loading

--- packages/components/src/html-elements/docs/headerPreview.tsx
+++ packages/components/src/html-elements/docs/headerPreview.tsx
@@ -1,17 +1,17 @@
-import { H1, Header, Main, Paragraph } from "@hopper-ui/components";
+import { H1, HtmlHeader, Main, Paragraph } from "@hopper-ui/components";
 
 export default function Example() {
     return (
-        <body>
-            <Header>
+        <>
+            <HtmlHeader>
                 <H1>Walking On The Moon</H1>
-            </Header>
+            </HtmlHeader>
             <Main color="neutral-weak">
                 <Paragraph>
                     Walking on the Moon is classic Sting moment, with his band The Police turning out one of
                     their most groovy reggae-inspired hits to the background of space exploration.
                 </Paragraph>
             </Main>
-        </body>
+        </>
     );
 }
