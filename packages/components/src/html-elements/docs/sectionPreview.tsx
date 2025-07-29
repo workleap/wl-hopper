92c3aa397c677819fcd7c3a3441f9d4b9eae311f
Victor Trinh <132942359+victortrinh2@users.noreply.github.com>
2025-07-29 10:41:11 -0400

    fix: Documentation page not loading (#723)

--- packages/components/src/html-elements/docs/sectionPreview.tsx
+++ packages/components/src/html-elements/docs/sectionPreview.tsx
@@ -1,16 +1,13 @@
-import { H1, H2, HtmlSection, Paragraph } from "@hopper-ui/components";
+import { H2, HtmlSection, Paragraph } from "@hopper-ui/components";
 
 export default function Example() {
     return (
-        <body>
-            <H1>Blue Origin</H1>
-            <HtmlSection color="neutral-weak">
-                <H2>Our vision</H2>
-                <Paragraph>
+        <HtmlSection color="neutral-weak">
+            <H2>Our vision</H2>
+            <Paragraph>
                     Blue Origin was founded by Jeff Bezos with the vision of enabling a future where
                     millions of people are living and working in space to benefit Earth.
-                </Paragraph>
-            </HtmlSection>
-        </body>
+            </Paragraph>
+        </HtmlSection>
     );
 }
