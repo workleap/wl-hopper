4d9b684e1a60ac1162bc94cd2339c7372bf84854
Victor Trinh <victor.trinh@workleap.com>
2025-08-07 16:12:43 -0400

    Clean up

--- apps/docs/app/ui/components/dosAndDonts/DosAndDonts.stories.tsx
+++ apps/docs/app/ui/components/dosAndDonts/DosAndDonts.stories.tsx
@@ -11,17 +11,47 @@ type Story = StoryObj<typeof meta>;
 
 export const Default = {
     args: {
-        dos: `
-    <Button>
-      <SparklesIcon />
-      <Text>Terms and conditions</Text>
-    </Button>
-        `,
-        donts: `
-    <Button>
-      <SparklesIcon />
-      Terms and conditions
-    </Button>
-        `
+        children: "When a Button contains anything other than a single text node (e.g., an icon, a spinner, or another element), the accompanying text must be wrapped in a `<Text>` component.",
+        dos: {
+            code: "<Button><SparklesIcon /><Text>Terms and conditions</Text></Button>"
+        },
+        donts: {
+            code: "<Button><SparklesIcon />Terms and conditions</Button>"
+        }
+    }
+} satisfies Story;
+
+export const OnlyCards = {
+    args: {
+        dos: {
+            explanation: "When a Button contains anything other than a single text node, the accompanying text must be wrapped in a `<Text>` component.",
+            code: "<Button><SparklesIcon /><Text>Terms and conditions</Text></Button>"
+        },
+        donts: {
+            explanation: "Use text without `<Text>` if you have anything other than a single text node inside the Button.",
+            code: "<Button><SparklesIcon />Terms and conditions</Button>"
+        }
+    }
+} satisfies Story;
+
+export const OnlyCode = {
+    args: {
+        dos: {
+            code: "<Button><SparklesIcon /><Text>Terms and conditions</Text></Button>"
+        },
+        donts: {
+            code: "<Button><SparklesIcon />Terms and conditions</Button>"
+        }
+    }
+} satisfies Story;
+
+export const OnlyText = {
+    args: {
+        dos: {
+            explanation: "When a Button contains anything other than a single text node, the accompanying text must be wrapped in a `<Text>` component."
+        },
+        donts: {
+            explanation: "Use text without `<Text>` if you have anything other than a single text node inside the Button."
+        }
     }
 } satisfies Story;
--- apps/docs/app/ui/components/dosAndDonts/DosAndDonts.tsx
+++ apps/docs/app/ui/components/dosAndDonts/DosAndDonts.tsx
@@ -6,59 +6,112 @@ import "./dosAndDonts.css";
 import { formatCode } from "@/app/lib/formatingCode";
 import { HighlightCode } from "@/components/highlightCode";
 import { ThemeContext } from "@/context/theme/ThemeProvider";
-import { HopperProvider, Tag, Text, type ColorScheme } from "@hopper-ui/components";
+import { HopperProvider, Tag, Text, type ColorScheme, type TagProps } from "@hopper-ui/components";
 import { CheckmarkIcon, DismissIcon } from "@hopper-ui/icons";
-import { useContext, useEffect, useState, type ComponentProps } from "react";
+import pluginBabel from "prettier/plugins/babel";
+import pluginEstree from "prettier/plugins/estree";
+import pluginTypescript from "prettier/plugins/typescript";
+import prettier from "prettier/standalone";
+import { useContext, useEffect, useState, type ComponentProps, type ReactNode } from "react";
 import Card from "../card/Card";
 
-export interface DosAndDontsProps extends Omit<ComponentProps<"div">, "children">{
-    dos: string;
-    donts: string;
+type Variant = "do" | "dont";
+
+interface DosAndDontsCardProps {
+    icon: ReactNode;
+    variant: TagProps["variant"];
+    textValue: string;
 }
 
-function DosAndDonts({ dos, donts, className }: DosAndDontsProps) {
+const VariantToCard: Record<Variant, DosAndDontsCardProps> = {
+    "do": {
+        icon: <CheckmarkIcon />,
+        variant: "positive",
+        textValue: "Do"
+    },
+    "dont": {
+        icon: <DismissIcon />,
+        variant: "negative",
+        textValue: "Don't"
+    }
+};
+
+interface DosAndDontsItem {
+    explanation?: string;
+    code?: string;
+}
+
+export interface DosAndDontsProps extends Omit<ComponentProps<"div">, "children"> {
+    children?: ReactNode;
+    dos?: DosAndDontsItem;
+    donts?: DosAndDontsItem;
+}
+
+function DosAndDonts({ children, dos, donts, className }: DosAndDontsProps) {
     const { colorMode } = useContext(ThemeContext);
     const theme = colorMode as ColorScheme;
     const dosAndDontsClass = clsx("hd-dosAndDonts", className);
 
-    const [doCode, setDoCode] = useState("");
-    const [dontCode, setDontCode] = useState("");
-
-    useEffect(() => {
-        const formatCodes = async () => {
-            try {
-                const formattedDoCode = await formatCode(dos, "tsx");
-                const formattedDontCode = await formatCode(donts, "tsx");
-
-                setDoCode(formattedDoCode);
-                setDontCode(formattedDontCode);
-            } catch (error) {
-                console.error("Error formatting code:", error);
-            }
-        };
-
-        formatCodes();
-    }, [dos, donts]);
-
     const renderCard = (variant: "do" | "dont") => {
-        const code = variant === "do" ? doCode : dontCode;
+        const item = variant === "do" ? dos : donts;
+
+        if (!item) {
+            return null;
+        }
+
+        const cardProps = VariantToCard[variant];
+        const { icon, variant: tagVariant, textValue } = cardProps;
+        const { explanation, code } = item;
 
         return (
             <Card size="sm" className="hd-dosAndDonts__card">
-                <Tag variant={variant === "do" ? "positive" : "negative"}>
-                    {variant === "do" ? <CheckmarkIcon /> : <DismissIcon />}
-                    <Text size="sm">{variant === "do" ? "Do" : "Don't"}</Text>
+                <Tag variant={tagVariant} textValue={textValue}>
+                    {icon}
+                    <Text size="sm">{textValue}</Text>
                 </Tag>
-                <HighlightCode code={code} />
+                {explanation}
+                {code && <Example code={code} />}
             </Card>
         );
     };
 
     return (
         <HopperProvider colorScheme={theme} className={dosAndDontsClass}>
-            {renderCard("do")}
-            {renderCard("dont")}
+            {children}
+            <div className="hd-dosAndDonts__explanation">
+                {renderCard("do")}
+                {renderCard("dont")}
+            </div>
         </HopperProvider>
+    );
+}
+
+interface ExampleProps {
+    code: string;
+}
+
+function Example({ code }: ExampleProps) {
+    const [formattedCode, setFormattedCode] = useState("");
+
+    useEffect(() => {
+        const format = async () => {
+            const prettierCode = await prettier.format(code, {
+                parser: "babel-ts",
+                plugins: [pluginBabel, pluginTypescript, pluginEstree],
+                semi: true,
+                tabWidth: 2
+            });
+
+            const mdxCode = await formatCode(prettierCode.trimEnd(), "tsx");
+
+            setFormattedCode(mdxCode);
+        };
+
+        format();
+    }, [code]);
+
+    return (
+        <HighlightCode code={formattedCode} />
     );
 }
 
