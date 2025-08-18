aacc368e9d8343dcd13a6df93b327f207e697fa6
Victor Trinh <victor.trinh@workleap.com>
2025-08-18 16:30:18 -0400

    Fix look

--- apps/docs/app/ui/components/dosAndDonts/DosAndDonts.ai.tsx
+++ apps/docs/app/ui/components/dosAndDonts/DosAndDonts.ai.tsx
@@ -29,7 +29,7 @@ function DosAndDonts({ items }: DosAndDontsProps) {
             {items.map((item, index) => (
                 // eslint-disable-next-line react/no-array-index-key
                 <div key={index}>
-                    <span>Rule: {index + 1}</span>
+                    Rule {index + 1}
                     <ul>
                         {item.do && (
                             <li>
