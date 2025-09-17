31c90d9f7dab1d33905d2c14f75affd7c766ba29
Victor Trinh <victor.trinh@workleap.com>
2025-09-17 17:31:31 -0400

    Update react-router-dom

--- packages/components/setupTests.ts
+++ packages/components/setupTests.ts
@@ -2,6 +2,15 @@ import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/jest-globals";
 import failOnConsole from "jest-fail-on-console";
 import { enableFetchMocks } from "jest-fetch-mock";
+import { TextDecoder, TextEncoder } from "util";
+
+// Add TextEncoder and TextDecoder to the global scope
+// These are required by react-router-dom
+// @ts-expect-error global variables
+global.TextEncoder = TextEncoder;
+
+// @ts-expect-error global variables
+global.TextDecoder = TextDecoder;
 
 // This is used for tests with react-router-dom.
 // There was an error thrown "ReferenceError: Request is not defined"
@@ -9,4 +18,5 @@ enableFetchMocks();
 
 // This will fail the test if there is a console.error or console.warn
 failOnConsole();
+
 
