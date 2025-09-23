9f03123ea3e342d95f140ddfe0825b5aba10d989
Victor Trinh <victor.trinh@workleap.com>
2025-09-23 11:09:12 -0400

    Add better comments

--- packages/components/setupTests.ts
+++ packages/components/setupTests.ts
@@ -4,8 +4,14 @@ import failOnConsole from "jest-fail-on-console";
 import { enableFetchMocks } from "jest-fetch-mock";
 import { TextDecoder, TextEncoder } from "util";
 
-// Add TextEncoder and TextDecoder to the global scope
-// These are required by react-router-dom
+/**
+ * React Router (and some of its transitive deps) expect the Web Encoding API
+ * (TextEncoder / TextDecoder) to exist on the global object. jsdom does not
+ * provide them, so we patch them in from Node’s util. This makes our test
+ * environment behave closer to a browser. Once we migrate from jsdom to
+ * happy-dom, this workaround will no longer be needed since happy-dom
+ * includes these globals out of the box.
+ */
 global.TextEncoder = TextEncoder as typeof global.TextEncoder;
 global.TextDecoder = TextDecoder as typeof global.TextDecoder;
 
