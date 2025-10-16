ad4c1fe94b91392affdf634fa7ec0e04e6342f8e
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-10-16 14:57:12 -0400

    add useDebounce hook

---
+++ packages/components/src/utils/src/useDebounce.ts
@@ -0,0 +1,64 @@
+import { useCallback, useEffect, useRef, useState, type Dispatch, type SetStateAction } from "react";
+
+const DebounceTimeouts = {
+    sm: 100,
+    md: 400,
+    lg: 1000
+} as const;
+
+/**
+ * This is accepting either one of the predefined debounce timeouts or a custom number of milliseconds.
+ */
+type SuggestedDebouncedWaitTimes = keyof typeof DebounceTimeouts | (number & {});
+
+export const useDebounceCallback = <CallbackArgs extends unknown[]>(
+    callback: (...args: CallbackArgs) => void,
+    wait: SuggestedDebouncedWaitTimes = "sm",
+    leading = false
+): ((...args: CallbackArgs) => void) => {
+    const storedCallback = useRef(callback);
+    useEffect(() => {
+        storedCallback.current = callback;
+    });
+
+    const timeout = useRef<ReturnType<typeof setTimeout>>();
+
+    useEffect(
+        () => () => {
+            timeout.current && clearTimeout(timeout.current);
+            timeout.current = undefined;
+        },
+        [wait, leading, storedCallback]
+    );
+
+    return useCallback((...args: CallbackArgs) => {
+        const { current } = timeout;
+        const waitInMs = typeof wait === "number" ? wait : DebounceTimeouts[wait];
+        if (current === undefined && leading) {
+            timeout.current = setTimeout(() => {
+                timeout.current = undefined;
+            }, waitInMs);
+
+            return storedCallback.current.apply(null, args);
+        }
+
+        current && clearTimeout(current);
+
+        timeout.current = setTimeout(() => {
+            timeout.current = undefined;
+            storedCallback.current.apply(null, args);
+        }, waitInMs);
+    }, [wait, leading, storedCallback]);
+};
+
+export type UseDebounceResult<TState> = [
+    TState,
+    Dispatch<SetStateAction<TState>>,
+    Dispatch<SetStateAction<TState>>
+];
+
+export function useDebounce<TState>(initialState: TState | (() => TState), wait?: SuggestedDebouncedWaitTimes, leading?: boolean): UseDebounceResult<TState> {
+    const state = useState(initialState);
+
+    return [state[0], useDebounceCallback(state[1], wait, leading), state[1]] ;
+}
---
+++ packages/components/src/utils/tests/jest/useDebounce.test.ts
@@ -0,0 +1,90 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+
+import { useDebounce, useDebounceCallback } from "../../src/useDebounce.ts";
+
+describe("useDebounce()", () => {
+    jest.useFakeTimers();
+
+    it("should wait 100ms before invoking the callback", () => {
+        const cb = jest.fn();
+        const { result } = renderHook(() => useDebounceCallback(cb));
+
+        act(result.current);
+        act(result.current);
+        act(result.current);
+
+        act(() => jest.advanceTimersByTime(50));
+        expect(cb).toHaveBeenCalledTimes(0);
+        act(() => jest.advanceTimersByTime(100));
+        expect(cb).toHaveBeenCalledTimes(1);
+
+        act(result.current);
+        act(result.current);
+        act(result.current);
+
+        act(() => jest.advanceTimersByTime(99));
+        expect(cb).toHaveBeenCalledTimes(1);
+        act(() => jest.advanceTimersByTime(1));
+        expect(cb).toHaveBeenCalledTimes(2);
+    });
+
+    it("should invoke the callback on the leading edge", () => {
+        const cb = jest.fn();
+        const { result } = renderHook(() => useDebounceCallback(cb, 100, true));
+
+        act(result.current);
+        expect(cb).toHaveBeenCalledTimes(1);
+
+        act(result.current);
+        act(result.current);
+        act(result.current);
+        expect(cb).toHaveBeenCalledTimes(1);
+
+        act(() => jest.advanceTimersByTime(100));
+        expect(cb).toHaveBeenCalledTimes(2);
+
+        act(result.current);
+        expect(cb).toHaveBeenCalledTimes(3);
+
+        act(result.current);
+        act(result.current);
+        act(result.current);
+
+        expect(cb).toHaveBeenCalledTimes(3);
+        act(() => jest.advanceTimersByTime(100));
+        expect(cb).toHaveBeenCalledTimes(4);
+    });
+
+    it("should set the last value the callback was invoked with after 100ms", () => {
+        const { result } = renderHook(() => useDebounce(1, 100));
+
+        act(() => result.current[1](2));
+        expect(result.current[0]).toBe(1);
+
+        act(() => result.current[1](3));
+        act(() => jest.advanceTimersByTime(50));
+
+        act(() => result.current[1](4));
+        act(() => jest.advanceTimersByTime(50));
+        expect(result.current[0]).toBe(1);
+
+        act(() => jest.advanceTimersByTime(50));
+        expect(result.current[0]).toBe(4);
+    });
+
+    it("should set the value on the leading edge", () => {
+        const { result } = renderHook(() => useDebounce(1, 100, true));
+
+        act(() => result.current[1](2));
+        expect(result.current[0]).toBe(2);
+
+        act(() => result.current[1](3));
+        act(() => result.current[1](4));
+
+        act(() => jest.advanceTimersByTime(50));
+        expect(result.current[0]).toBe(2);
+
+        act(() => jest.advanceTimersByTime(50));
+        expect(result.current[0]).toBe(4);
+    });
+});
