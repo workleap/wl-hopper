a10a67d0132f46cfaf0effb50401b94ec5468076
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-10-21 12:30:51 -0400

    update example

--- packages/components/src/date-picker/docs/date-range-picker/custom-validation.tsx
+++ packages/components/src/date-picker/docs/date-range-picker/custom-validation.tsx
@@ -1,18 +1,23 @@
-import { DateRangePicker } from "@hopper-ui/components";
+import { Button, DateRangePicker, Form } from "@hopper-ui/components";
 import { getLocalTimeZone, today } from "@internationalized/date";
 
 export default function Example() {
     return (
-        <DateRangePicker
-            label="Trip dates"
-            validate={range =>
-                range?.end.compare(range.start) > 7
-                    ? "Maximum stay duration is 1 week."
-                    : null}
-            defaultValue={{
-                start: today(getLocalTimeZone()),
-                end: today(getLocalTimeZone()).add({ weeks: 1, days: 3 })
-            }}
-        />
+        <Form>
+            <DateRangePicker
+                label="Trip dates"
+                validate={range =>
+                    range?.end.compare(range.start) > 7
+                        ? "Maximum stay duration is 1 week."
+                        : null}
+                defaultValue={{
+                    start: today(getLocalTimeZone()),
+                    end: today(getLocalTimeZone()).add({ weeks: 1, days: 3 })
+                }}
+            />
+            <Button type="submit" variant="primary">
+                Submit
+            </Button>
+        </Form>
     );
 }
