8d2158edf04d393ce5117069a48ab54530c36a64
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-10-27 13:04:29 -0400

    remove debug story

--- packages/components/src/tag/tests/chromatic/Tag.stories.tsx
+++ packages/components/src/tag/tests/chromatic/Tag.stories.tsx
@@ -391,26 +391,3 @@ export const TagWithTooltip = {
         );
     }
 } satisfies Story;
-
-export const Debugging = {
-    parameters: {
-        ...hopperParameters({
-            colorSchemes: ["light"]
-        })
-    },
-    render: props => {
-        return (
-            <Tag
-                href="https://www.google.com"
-                id="1"
-                size="sm"
-                excludeFromTabOrder
-                onPress={() => window.alert("test")}
-                onHoverStart={() => console.log("ON HOVER")}
-                {...props}
-            >
-                Tag 1
-            </Tag>
-        );
-    }
-} satisfies Story;
