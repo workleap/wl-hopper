f0a31792734a21113f0ca78cf8de1c942abf182b
Alexandre Asselin <alexandre.asselin@gsoft.com>
2026-01-30 22:08:45 -0500

    fix issues

--- packages/components/src/utils/src/EnsureTextWrapper.tsx
+++ packages/components/src/utils/src/EnsureTextWrapper.tsx
@@ -2,11 +2,11 @@ import type { ReactNode } from "react";
 
 import { Text } from "../../typography/index.ts";
 
+import { isNil } from "./assertion.ts";
 import { isTextOnlyChildren } from "./isTextOnlyChildren.ts";
 
 export function ensureTextWrapper(children: ReactNode, elementType?: string): ReactNode {
-    // Can't do only "children && ..." because children could be 0
-    if (children !== undefined && isTextOnlyChildren(children)) {
+    if (!isNil(children) && isTextOnlyChildren(children)) {
         return <Text elementType={elementType}>{children}</Text>;
     }
 
