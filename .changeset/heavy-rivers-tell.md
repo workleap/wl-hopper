b166e850dc9d563edd7028b5ac7f86904adc32b3
Alexandre Asselin <alexasselin008@gmail.com>
2026-01-30 22:20:09 -0500

    fix: Two small fix for protect (#874)

---
+++ .changeset/heavy-rivers-tell.md
@@ -0,0 +1,5 @@
+---
+"@hopper-ui/components": patch
+---
+
+Add overflow-wrap: anywhere; to tooltip to prevent overflow
--- packages/components/src/utils/src/EnsureTextWrapper.tsx
+++ packages/components/src/utils/src/EnsureTextWrapper.tsx
@@ -2,10 +2,11 @@ import type { ReactNode } from "react";
 
 import { Text } from "../../typography/index.ts";
 
+import { isNil } from "./assertion.ts";
 import { isTextOnlyChildren } from "./isTextOnlyChildren.ts";
 
 export function ensureTextWrapper(children: ReactNode, elementType?: string): ReactNode {
-    if (children && isTextOnlyChildren(children)) {
+    if (!isNil(children) && isTextOnlyChildren(children)) {
         return <Text elementType={elementType}>{children}</Text>;
     }
 
