6f158e6ed751047ea293aa4906d565e2a4feb42b
Tommy Josépovic <44372776+tjosepo@users.noreply.github.com>
2026-02-02 12:12:43 -0500

    Improve typing of useColorSchemeValue

--- packages/styled-system/src/color-scheme/useColorSchemeValue.ts
+++ packages/styled-system/src/color-scheme/useColorSchemeValue.ts
@@ -1,6 +1,6 @@
 import { useColorSchemeContext } from "./ColorSchemeContext.ts";
 
-export function useColorSchemeValue(lightColor: string, darkColor: string) {
+export function useColorSchemeValue<L extends string, D extends string>(lightColor: L, darkColor: D): L | D {
     const { colorScheme } = useColorSchemeContext();
 
     return colorScheme === "dark" ? darkColor : lightColor;
