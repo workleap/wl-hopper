5790abf0d8695155caedee704ba6f035b9ad2c65
Mahmoud Moravej <55846417+mahmoudmoravej@users.noreply.github.com>
2026-04-17 13:44:48 -0400

    feat(tokens): update button branding tokens (#918)
    Co-authored-by: Tommy Josépovic <44372776+tjosepo@users.noreply.github.com>
    Co-authored-by: Mahmoud Moravej <mahmoud.moravej@workleap.com>

--- packages/styled-system/src/tokens/generated/styledSystemToTokenMappings.ts
+++ packages/styled-system/src/tokens/generated/styledSystemToTokenMappings.ts
@@ -1093,7 +1093,6 @@ export const Elevation = {
     "core_sm": "shadow-sm",
     "core_md": "shadow-md",
     "core_lg": "shadow-lg",
-    "core_tactility-button": "shadow-tactility-button",
     "none": "elevation-none",
     "raised": "elevation-raised",
     "lifted": "elevation-lifted",
